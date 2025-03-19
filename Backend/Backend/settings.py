cb48a4e7181678c1c2f61bf3034b247679c57f8d
Trung Du <nguyentrinhtrungdu@gmail.com>
2025-03-19 23:17:51 +0700

    UserModel

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'User',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +52,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Backend.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ShopWeb
+python manage.py startapp users
+python manage.py runserver
+# Tạo migration cho model User
+python manage.py makemigrations
+
+# Áp dụng migration để tạo bảng trong cơ sở dữ liệu
+python manage.py migrate
+
+
+python manage.py makemigrations User
